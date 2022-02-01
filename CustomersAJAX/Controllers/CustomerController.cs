506e2624a85c7e55464d49869dc16821aaf3d832
Julian Gallo Londono <julian.gallol@perficient.com>
2022-02-01 16:55:10 -0500

    Add project files.

---
+++ CustomersAJAX/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using CustomersAJAX.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CustomersAJAX.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer customer;
+        List<Customer> customers;
+
+        public CustomerController()
+        {
+            customers = new List<Customer>();
+            customers.Add(new Customer(0, "Julian", 32));
+            customers.Add(new Customer(1, "Tatiana", 32));
+            customers.Add(new Customer(2, "Liliana", 54));
+            customers.Add(new Customer(3, "Oscar", 50));
+            customers.Add(new Customer(4, "Sara", 26));
+            customers.Add(new Customer(5, "Javier", 72));
+        }
+
+
+        public ActionResult Index()
+        {
+            Tuple<List<Customer>, Customer> tuple;
+            tuple = new Tuple<List<Customer>, Customer>(customers, customers[0]);
+
+
+            return View("Customer", tuple);
+        }
+
+        public ActionResult OnSelectCustomer(string CustomerNumber)
+        {
+            Tuple<List<Customer>, Customer> tuple;
+            tuple = new Tuple<List<Customer>, Customer>(customers, customers[Convert.ToInt32(CustomerNumber)]);
+
+
+            return View("Customer", tuple);
+        }
+
+    }
+}
---
+++ CustomersAJAX/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomersAJAX.Models
+{
+    public class Customer
+    {
+        public Customer(int ID, string name, int age)
+        {
+            this.ID = ID;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int ID{ get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
